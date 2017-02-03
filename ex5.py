b62a4ff10c617fdb941564c4189f81399c762713
Stephan Raab <stephan.raab@nyhart.com>
2017-02-03 14:31:30 -0500

    convert the inches and pounds to centimeters and kilograms

--- ex5.py
+++ ex5.py
@@ -1,14 +1,17 @@
 name = 'Zed A. Shaw'
 age = 35 # not a lie
-height = 74 # inches
-weight = 180 # lbs
+height = 74.0 # inches
+weight = 180.0 # lbs
 eyes = 'Blue'
 teeth = 'White'
 hair = 'Brown'
 
+height_in_cm = height * 2.54
+weight_in_kg = weight * 0.45
+
 print "Let's talk about %s." % name
-print "He's %d inches tall." % height
-print "he's %d pounds heavy." % weight
+print "He's %d inches tall or %d centimeters tall." % (height, height_in_cm)
+print "He weighs %d pounds and %d kilograms." % (weight, weight_in_kg)
 print "Actually that's not too heavy."
 print "He's got %s eyes and %s hair." % (eyes, hair)
 print "His teeth are usually %s depending on the coffee." % teeth
