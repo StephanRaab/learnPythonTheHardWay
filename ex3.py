0adde69d210348f5edc1e8023f7412affa8a1164
Stephan Raab <stephan.raab@nyhart.com>
2017-02-03 13:35:50 -0500

    Rewrite ex3.py to use floating point numbers

---
+++ ex3.py
@@ -0,0 +1,25 @@
+print "I will now count my chickens:"
+
+#print hens with math after it
+print "Hens", 25.0 + 30.0 / 6.0
+#print roosteres with math after it
+print "Roosters", 100.0 - 25.0 * 3.0 % 4.0
+#print line
+print "Now I will count the eggs:"
+#do math
+print 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
+#first comparison operator
+print "Is it true that 3 + 2 < 5 - 7?"
+print 3.0 + 2.0 < 5.0 - 7.0
+#math
+print "What is 3 + 2?", 3.0 + 2.0
+#more math
+print "What is 5 - 7?", 5.0 - 7.0
+#now we see why the boolean operator returned false earlier
+print "Oh, that's why it's False." 
+#print line
+print "How about some more."
+#extra comparisons between the two numbers
+print "Is it greater?", 5.0 > -2.0
+print "Is it greater or equal?", 5.0 >= -2.0
+print "Is it less or equal?", 5.0 <= -2.0
